53970f6ba7d81e85281be939cbd37c9febe37b39
Geoffrey Hayes <hayesgm@gmail.com>
2026-03-26 17:36:44 -0700

    bump 0.0.3

--- Formula/legend-cli.rb
+++ Formula/legend-cli.rb
@@ -2,12 +2,12 @@ class LegendCli < Formula
   desc "CLI and MCP server for Legend"
   homepage "https://legend.xyz"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     on_arm do
       url "https://github.com/legend-hq/legend-cli/releases/download/v#{version}/legend-cli-macos-aarch64.tar.gz"
-      sha256 "cc31068e1ae6c1bb8a69419cd9fdc5adf34d3dfe066b504733e31cda80e91427"
+      sha256 "56071da695be206ee60ebf76a8487d08f9069c8b52290d758a9100139917b780"
     end
   end
 
